93dcfd6a8bf7fd68aa5f7027c9123891c669adf3
Chris Marr <chris.marr@siemens.com>
2017-09-06 16:29:29 +0100

    Up-revved version number

--- etc/CommonAssemblyInfo.cs
+++ etc/CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Ciaran Downey and Contributors")]
 [assembly: AssemblyCopyright("Copyright Ciaran Downey 2017")]
 
-[assembly: AssemblyVersion("2.1.2")]
-[assembly: AssemblyFileVersion("2.1.2")]
-[assembly: AssemblyInformationalVersion("2.1.2-alpha")]
+[assembly: AssemblyVersion("2.1.3")]
+[assembly: AssemblyFileVersion("2.1.3")]
+[assembly: AssemblyInformationalVersion("2.1.3-alpha")]
